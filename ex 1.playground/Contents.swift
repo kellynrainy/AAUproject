432663a882eb0f695f0ebe8d4dd0f527d13ddd3e
YIWEN LIANG <kellynrainy@gmail.com>
2018-02-20 17:16:07 -0800

    ex 1

---
+++ ex 1.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var myString = "Hello, World!"
+
+print(myString)
+//Print “Hello, World!”
+
+
+
+let name = "yiwen"
+let age = 22
+print("Hello, my name is \(name) and I’m \(age) years old.")
+//Print “Hello, my name is (name) and I’m (age) years old.”.
+
+
+
+func printHelloWorld() -> String {
+    return "Hello, World!"
+}
+print(printHelloWorld())
+//Write a function that prints “Hello, World!”
+
+
+
+func Addition(x: Int, y: Int) -> Int
+{
+    return x + y
+}
+
+print(Addition(x:10, y: 20))
+//func printAddition(_ x: Int, _ y: Int)
