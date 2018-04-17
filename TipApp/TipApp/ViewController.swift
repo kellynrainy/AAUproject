1a66aa2ee8e8c9e6fcaf7bcfdd5c069867f5b249
Yiwen Liang <36465113+kellynrainy@users.noreply.github.com>
2018-04-17 16:59:28 -0700

    Add files via upload

---
+++ TipApp/TipApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by YIWEN LIANG on 4/17/18.
+//  Copyright © 2018 YIWEN LIANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
